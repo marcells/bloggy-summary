6eabcfa4cf9dd5cae4370e324461fdfc699ba741
Marcell Spies <marcells@gmx.de>
2014-05-07 22:35:01 +0200

    Added html encoding and limitation of summary length.

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 'use strict';
 
-var minidom = require('minidom');
+var minidom = require('minidom'),
+    htmlencode = require('node-htmlencode'),
+    makeSummary = function (content, maxLength, prefix, suffix) {
+        var summary = content
+            .substr(0, content.lastIndexOf(' ', maxLength));
+
+        return prefix + htmlencode.htmlEncode(summary).replace(/&#10;/g, '<br />') + suffix;
+    };
+
+exports.maxLength = 300;
+exports.prefix = '<p>';
+exports.suffix = ' ...<p>';
 
 exports.init = function (engine) {
     var original = engine.loadContent;
 
-    engine.loadContent = function (content, callback) {
+    engine.loadContent = function (entry, content, callback) {
         original(content, function (transformedContent) {
-            var dom = minidom(transformedContent);
+            var dom = minidom('<div>' + transformedContent + '</div>'),
+                text = dom.children[0].textContent;
 
-            console.log("Yeah!!")
-            console.log(dom.textContent);
+            entry.summary = makeSummary(text, exports.maxLength, exports.prefix, exports.suffix);
 
             callback(transformedContent);
         });
